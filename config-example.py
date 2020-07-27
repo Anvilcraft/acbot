4ed7df800391dc7f85a47e24b74ac39805049633
Jonas Leder <jonas@jonasled.de>
2020-07-27 14:55:44 +0200

    added example config

---
+++ config-example.py
@@ -0,0 +1,22 @@
+prefix = "!" #the prefix for the bot commands
+token = "" #the login token of the bot
+
+sqlServer = ""
+sqlUser = ""
+sqlPassword = ""
+sqlDatabase = ""
+
+roleMap = {
+"tilera" : "tilera",
+"Server Administration" : "admin",
+"Anvilcraft Team" : "team",
+"Moderator" : "mod",
+"Mod Dev" : "dev",
+"Modpack Creator" : "modpack",
+"YOUTUBER" : "youtube",
+"Streamer" : "stream"
+}
+
+ignoreRoles = [
+    "Bot"
+]
